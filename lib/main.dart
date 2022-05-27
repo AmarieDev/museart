7f69472fb1a8b9fcb0e75b508a6294d5a4d6609f
ammar <mresh.dev@gmail.com>
2022-05-27 21:32:47 +0200

    Theme und Scaffold erstellt

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // home: HomeScreen(),
-      home: Text("Hello World!"),
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          // Define the default brightness and colors.
+          //brightness: Brightness.dark,
+          primaryColor: Colors.lightBlue[800],
+          colorScheme:
+              ColorScheme.fromSwatch().copyWith(secondary: Colors.amber),
+
+          // Define the default font family.
+          //fontFamily: 'Georgia',
+
+          // Define the default `TextTheme`. Use this to specify the default
+          // text styling for headlines, titles, bodies of text, and more.
+          textTheme: const TextTheme(
+            headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+            headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+            bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+          ),
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text(""),
+          ),
+          body: const Text("Hello World!"),
+        ));
   }
 }
