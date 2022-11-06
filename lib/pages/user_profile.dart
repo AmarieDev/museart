d1d322ea640ae96b390bca2091cb3b647c31e342
ammar <mresh.dev@gmail.com>
2022-11-06 18:46:32 +0100

    user-profile: started user profile

--- lib/pages/user_profile.dart
+++ lib/pages/user_profile.dart
@@ -1,73 +1,90 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_application/reusable_widgets/my_padding.dart';
+import 'package:flutter_application/reusable_widgets/my_text_field.dart';
 
-class UserProfile extends StatefulWidget {
-  const UserProfile({Key? key}) : super(key: key);
-
+class EditProfilePage extends StatefulWidget {
   @override
-  State<UserProfile> createState() => _UserProfileState();
+  _EditProfilePageState createState() => _EditProfilePageState();
 }
 
-class _UserProfileState extends State<UserProfile> {
+class _EditProfilePageState extends State<EditProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0.0,
-        ),
-        body: Stack(
-          alignment: Alignment.center,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(20),
-                  // ignore: unnecessary_const
-                  child: const Text(
-                    "Profile",
-                    style: TextStyle(
-                      fontSize: 35,
-                      letterSpacing: 1.5,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
+      body: Container(
+        padding: const EdgeInsets.only(left: 16, top: 25, right: 16),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: ListView(
+            children: [
+              Center(
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(10.0),
+                      width: MediaQuery.of(context).size.width / 4,
+                      height: MediaQuery.of(context).size.width / 4,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/david.jpg'),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(10.0),
-                  width: MediaQuery.of(context).size.width / 2,
-                  height: MediaQuery.of(context).size.width / 2,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 5,
+                    Positioned(
+                      bottom: 15,
+                      right: 15,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.black54,
+                        child: IconButton(
+                          splashColor: const Color(0xffFF8383),
+                          hoverColor: const Color(0xff81BDFF),
+                          splashRadius: 500,
+                          icon: const Icon(
+                            Icons.edit,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
                     ),
-                    shape: BoxShape.circle,
-                    color: Colors.white,
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/ammar.jpg'),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 270, left: 184),
-              child: CircleAvatar(
-                backgroundColor: Colors.black54,
-                child: IconButton(
-                  icon: Icon(
-                    Icons.edit,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {},
+                  ],
                 ),
               ),
-            )
-          ],
-        ));
+              const SizedBox(
+                height: 35,
+              ),
+              MyPadding(
+                  child: MyTextField(
+                hintText: "Ammar",
+                readOnly: true,
+              )),
+              MyPadding(
+                  child: MyTextField(
+                hintText: "Mresh",
+                readOnly: true,
+              )),
+              MyPadding(
+                  child: MyTextField(
+                hintText: "ammar.mresh@gmail.com",
+                readOnly: true,
+              )),
+              MyPadding(
+                  child: MyTextField(
+                hintText: "04.05.1998",
+                readOnly: true,
+              )),
+              const SizedBox(
+                height: 35,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
