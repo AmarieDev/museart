7cc1c96406974cc4020b60b167bf454209c3cf5d
ammar <mresh.dev@gmail.com>
2022-12-07 16:31:45 +0100

    state-management: rapped the root widget with the change-notifier-provider and listened to it in the jams page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/pages/jams_detail_page.dart';
 import 'pages/sign_in.dart';
+import './providers/jams_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   runApp(const MyApp());
@@ -10,39 +13,45 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // Define the default brightness and colors.
-        //brightness: Brightness.dark,
-        primaryColor: const Color(0xff3E99FF),
+    return ChangeNotifierProvider(
+      create: (context) => JamsProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          // Define the default brightness and colors.
+          //brightness: Brightness.dark,
+          primaryColor: const Color(0xff3E99FF),
 
-        colorScheme: ColorScheme.fromSwatch()
-            .copyWith(secondary: const Color(0xffFF8383)),
+          colorScheme: ColorScheme.fromSwatch()
+              .copyWith(secondary: const Color(0xffFF8383)),
 
-        // Define the default font family.
-        //fontFamily: 'Georgia',
+          // Define the default font family.
+          //fontFamily: 'Georgia',
 
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            //primary: Colors.lightBlue[800],
-            primary: const Color(0xff3E99FF),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              //primary: Colors.lightBlue[800],
+              primary: const Color(0xff3E99FF),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
             ),
           ),
-        ),
-        textTheme: const TextTheme(
-          headline1: TextStyle(
-            color: Colors.black,
-            fontSize: 24.0,
-            fontWeight: FontWeight.w900,
+          textTheme: const TextTheme(
+            headline1: TextStyle(
+              color: Colors.black,
+              fontSize: 24.0,
+              fontWeight: FontWeight.w900,
+            ),
+            headline6: TextStyle(fontSize: 18.0, fontStyle: FontStyle.italic),
+            bodyText2: TextStyle(fontSize: 14.0),
           ),
-          headline6: TextStyle(fontSize: 18.0, fontStyle: FontStyle.italic),
-          bodyText2: TextStyle(fontSize: 14.0),
         ),
+        home: const SignIn(),
+        routes: {
+          JamDetailPage.routName: (context) => JamDetailPage(),
+        },
       ),
-      home: const Scaffold(body: SignIn()),
     );
   }
 }
--- lib/pages/jams_page.dart
+++ lib/pages/jams_page.dart
@@ -1,13 +1,40 @@
 import "package:flutter/material.dart";
+import 'package:flutter_application/pages/jams_detail_page.dart';
+import 'package:flutter_application/providers/jams_provider.dart';
+import '../data_models/jam.dart';
+import "package:provider/provider.dart";
 
 class JamsPage extends StatelessWidget {
-  const JamsPage({Key? key}) : super(key: key);
+  JamsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final jamsData = Provider.of<JamsProvider>(context);
+    final jams = jamsData.jams;
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: jams.length,
+          itemBuilder: (ctx, i) => GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(
+                JamDetailPage.routName,
+                arguments: jams[i].id,
+              );
+            },
+            child: ListTile(
+              leading: IconButton(
+                icon: Icon(Icons.music_note),
+                onPressed: () {},
+              ),
+              title: Text(jams[i].title),
+              subtitle: Text(jams[i].description),
+              trailing:
+                  Text("Date: " + jams[i].date + " Time: " + jams[i].time),
+            ),
+          ),
+        ),
       ),
     );
   }
