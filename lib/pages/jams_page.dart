92ab0c56e6b57c6764b6d0a24b47b8a589f1374d
ammar <mresh.dev@gmail.com>
2022-12-07 13:52:59 +0100

    added jams page

---
+++ lib/pages/jams_page.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+
+class JamsPage extends StatelessWidget {
+  const JamsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(8),
+      ),
+    );
+  }
+}
