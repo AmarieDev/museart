23c80f1fdb32be15c7315d80172cb634e35203f7
ammar <mresh.dev@gmail.com>
2022-12-08 15:51:16 +0100

    Firebase: modifications to create jam and add jam method

--- lib/providers/jams_provider.dart
+++ lib/providers/jams_provider.dart
@@ -7,7 +7,7 @@ class JamsProvider with ChangeNotifier {
   // ignore: prefer_final_fields
   List<Jam> _jams = [
     Jam(
-      id: 1,
+      id: " 1",
       title: "first jam",
       date: "05.06.2023",
       time: "15:30",
@@ -18,7 +18,7 @@ class JamsProvider with ChangeNotifier {
       prefreableInstruments: ["guitar", "drums"],
     ),
     Jam(
-      id: 2,
+      id: "2",
       title: " Rock and Roll",
       date: "01.02.2023",
       time: "15:30",
@@ -29,7 +29,7 @@ class JamsProvider with ChangeNotifier {
       prefreableInstruments: ["guitar", "drums"],
     ),
     Jam(
-      id: 3,
+      id: "3",
       title: "the ulti jam",
       date: "04.05.2023",
       time: "16:30",
@@ -45,7 +45,7 @@ class JamsProvider with ChangeNotifier {
     return [..._jams];
   }
 
-  Jam findById(int id) {
+  Jam findById(String id) {
     return _jams.firstWhere((element) => element.id == id);
   }
 
@@ -54,20 +54,33 @@ class JamsProvider with ChangeNotifier {
         "https://museart-351c7-default-rtdb.firebaseio.com/jams.json");
     http
         .post(
-          url,
-          body: json.encode({
-            'title': value.title,
-            'date': value.date,
-            'time': value.time,
-            'description': value.description,
-            'private': value.isPrivate,
-            'prefreable genres': value.prefreableGenres,
-            'prefreable insturments': value.prefreableInstruments
-          }),
-        )
-        .then((response) {});
-    _jams.add(value);
-    //notify every listener that a change has happen
-    notifyListeners();
+      url,
+      body: json.encode({
+        'title': value.title,
+        'date': value.date,
+        'time': value.time,
+        'location': value.location,
+        'description': value.description,
+        'private': value.isPrivate,
+        'prefreable genres': value.prefreableGenres,
+        'prefreable instruments': value.prefreableInstruments
+      }),
+    )
+        .then((response) {
+      final newJam = Jam(
+        id: json.decode(response.body)['name'],
+        title: value.title,
+        date: value.date,
+        time: value.time,
+        location: value.location,
+        description: value.description,
+        isPrivate: value.isPrivate,
+        prefreableGenres: value.prefreableGenres,
+        prefreableInstruments: value.prefreableInstruments,
+      );
+      _jams.add(newJam);
+      //notify every listener that a change has happen
+      notifyListeners();
+    });
   }
 }
