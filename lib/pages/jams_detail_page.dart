617c47bad38dbb8e110549f57ab2d93d05b06170
David <utomaniad@gmail.com>
2022-12-19 16:16:18 +0100

    gui: added padding in Jams detail page

--- lib/pages/jams_detail_page.dart
+++ lib/pages/jams_detail_page.dart
@@ -21,70 +21,73 @@ class JamDetailPage extends StatelessWidget {
       ),
       //
 
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(10.0),
-            width: MediaQuery.of(context).size.width / 5,
-            height: MediaQuery.of(context).size.width / 5,
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.white,
-              image: DecorationImage(
-                image: AssetImage('assets/images/david.jpg'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              width: MediaQuery.of(context).size.width / 5,
+              height: MediaQuery.of(context).size.width / 5,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+                image: DecorationImage(
+                  image: AssetImage('assets/images/david.jpg'),
+                ),
               ),
             ),
-          ),
-          Text('folding person | Host'),
-          SizedBox(
-            height: 30,
-            child: TextButton(
-              onPressed: () {},
-              child: const Text(
-                "go to Profile >",
-                style: TextStyle(color: Colors.black),
+            Text('folding person | Host'),
+            SizedBox(
+              height: 30,
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  "go to Profile >",
+                  style: TextStyle(color: Colors.black),
+                ),
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('Location'),
-              Text(
-                '1,6 km',
-              ),
-            ],
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height / 4,
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 0, 0, 0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Location'),
+                Text(
+                  '1,6 km',
+                ),
+              ],
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('Joined Users'),
-              Text(
-                '7/10',
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 5,
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 0, 0, 0),
               ),
-            ],
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height / 4,
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 0, 0, 0),
             ),
-          ),
-          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            SizedBox(width: MediaQuery.of(context).size.width),
-            Text('Genre: Rock, Metal'),
-            Text('Looking for: Dine Mama'),
-          ])
-        ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Joined Users'),
+                Text(
+                  '7/10',
+                ),
+              ],
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 5,
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+            ),
+            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              SizedBox(width: MediaQuery.of(context).size.width),
+              const Text('Genre: Rock, Metal'),
+              const Text('Looking for: Dine Mama'),
+            ])
+          ],
+        ),
       ),
 
       // floatingActionButton: FloatingActionButton(onPressed: () {}),
@@ -96,18 +99,21 @@ class JamDetailPage extends StatelessWidget {
           children: [
             Text(loadedJam.date + ' ' + loadedJam.time),
             Text(loadedJam.title),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(loadedJam.description),
-                ElevatedButton(
-                  child: Text(
-                    'Join',
-                    style: TextStyle(fontSize: 20.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(loadedJam.description),
+                  ElevatedButton(
+                    child: const Text(
+                      'Join',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                    onPressed: () {},
                   ),
-                  onPressed: () {},
-                ),
-              ],
+                ],
+              ),
             ),
           ],
         ),
