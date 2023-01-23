a008046e3272ab55d377e8b69bcc0effae6c2bc4
ammar <mresh.dev@gmail.com>
2023-01-23 14:26:31 +0100

    feature: pull to refresh in  jams page plus gui modifications

--- lib/pages/jams_page.dart
+++ lib/pages/jams_page.dart
@@ -16,33 +16,75 @@ class _JamsPageState extends State<JamsPage> {
   void initState() {
     Provider.of<JamsProvider>(context, listen: false).fetchJams();
     // work around if listen is set to true
-    //  Future.delayed(Duration.zero).then((value) =>
-    //      Provider.of<JamsProvider>(context, listen: false).fetchJams());
+    // Future.delayed(Duration.zero).then((value) =>
+    //    Provider.of<JamsProvider>(context, listen: false).fetchJams());
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     final jamsData = Provider.of<JamsProvider>(context);
+    Future<void> _refreshData() async {
+      // Fetch new data and update the state
+      setState(() {
+        Future.delayed(Duration.zero).then((value) =>
+            Provider.of<JamsProvider>(context, listen: false).fetchJams());
+      });
+    }
 
     final jams = jamsData.jams;
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.only(bottom: 0),
-        child: ListView.builder(
-          itemCount: jams.length,
-          itemBuilder: (ctx, i) => GestureDetector(
-            onTap: () async {
-              final isJoined = await jamsData.hasAlreadyJoined(jams[i].id);
-              Navigator.of(context).pushNamed(JamDetailPage.routeName,
-                  arguments: {'id': jams[i].id, 'isJoined': isJoined});
-            },
-            child: ListTile(
-              leading: const Icon(Icons.music_note),
-              title: Text(jams[i].title),
-              subtitle: Text(jams[i].description),
-              trailing:
-                  Text("Date: " + jams[i].date + " Time: " + jams[i].time),
+        child: RefreshIndicator(
+          onRefresh: _refreshData,
+          child: ListView.builder(
+            itemCount: jams.length,
+            itemBuilder: (ctx, i) => GestureDetector(
+              onTap: () async {
+                final isJoined = await jamsData.hasAlreadyJoined(jams[i].id);
+                Navigator.of(context).pushNamed(JamDetailPage.routeName,
+                    arguments: {'id': jams[i].id, 'isJoined': isJoined});
+              },
+              child: ListTile(
+                leading: const Icon(Icons.music_note),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: Text(
+                        jams[i].title,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text(
+                      jams[i].date,
+                      style: const TextStyle(fontSize: 14),
+                    ),
+                  ],
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12.0),
+                  child: Text(jams[i].description),
+                ),
+                trailing: Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Chip(
+                    padding: const EdgeInsets.all(0),
+                    label: Text(
+                        (jams[i].joinedUsers.length - 1).toString() +
+                            '/' +
+                            jams[i].maxJamers.toString(),
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 10)),
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+              ),
             ),
           ),
         ),
